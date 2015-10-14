38e4810817821f7bd54894035501da81a4c60c3d
ahmed_abdo <eng.ahmed.farid.87@hotmail.com>
2015-10-14 17:15:07 +0200

    rest services integration

---
+++ MyWayWeb/src/controller/restServices/parentMonitoring/PostDriverReportServlet.java
@@ -0,0 +1,92 @@
+package controller.restServices.parentMonitoring;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.DriverDashBoard;
+import model.UserProfile;
+
+import com.ibm.json.java.JSONArray;
+import com.ibm.json.java.JSONObject;
+
+import dao.ParentMonitoringDao;
+import dao.ParentMonitoringDaoImpl;
+import dao.UserProfileDoa;
+import dao.UserProfileDoaImpl;
+
+/**
+ * Servlet implementation class PostDriverReportServlet
+ */
+@WebServlet("/postDriverReport")
+public class PostDriverReportServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PostDriverReportServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		postDriverReport(request,response);
+	}
+
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		postDriverReport(request,response);
+	}
+	
+	private void postDriverReport(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+
+		String reportLan = request.getParameter("report_lon");
+		
+		String reportLat = request.getParameter("report_lat");
+		
+		String reportReason = request.getParameter("report_reason");
+		
+		String reportTome = request.getParameter("report_time");
+		
+		String reportDate = request.getParameter("report_date");
+		
+		String driverUserName = request.getParameter("username");
+		
+		
+	
+		UserProfileDoa userProfileDoa = new UserProfileDoaImpl();
+		UserProfile driverProfile = userProfileDoa.retriveUserProfile(driverUserName);
+		
+		//DriverDashBoard driverDashBoard = new DriverDashBoard(driverProfile, battaryStatus, currentLat, currentLon) ;
+		
+		JSONObject json = new JSONObject();
+		
+		//ParentMonitoringDao parentMonitoringDao = new ParentMonitoringDaoImpl();
+		//parentMonitoringDao.setDriverDashBoard(driverDashBoard);
+			
+		json.put("message", "success");
+		json.put("result_code", 0);
+		
+			
+		JSONArray result_data = new JSONArray();
+		json.put("result_data", result_data);
+		
+		
+		response.getWriter().print(json);
+		
+		
+	}
+
+}
